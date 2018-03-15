b2ead1d4faa2150d3a0f10144ab63dc3b2695e47
maks-1987 <max.moderation@gmail.com>
2018-03-15 15:32:56 +0200

    add BinarySearch

---
+++ src/main/java/ProcedurJava/LoopAndRecursion/Sort/BinarySearch1.java
@@ -0,0 +1,35 @@
+package ProcedurJava.LoopAndRecursion.Sort;
+/* В стандартной библиотеке Java уже имеется реализация двоичного
+поиска (который при этом может быть расширен через интерфейс Comparator).
+Для объектных типов данных общий вид метода выглядит так:
+java.util.Arrays.binarySearch(T[] array, T value[, Comparator c])
+ */
+class BinarySearch1 {
+    private double[] array;
+
+    BinarySearch1(double[] array) {
+        this.array = array;
+    }
+    // алгоритм поиска
+    int find(double x) {
+        int i = -1;
+        if (this.array != null) {
+            int low = 0, high = this.array.length, mid;
+            while (low < high) {
+                mid = (low + high) / 2; // Можно заменить на:
+                // (low + high) >>> 1, чтоб не возникло переполнение целого
+                if (x == this.array[mid]) {
+                    i = mid;
+                    break;
+                } else {
+                    if (x < this.array[mid]) {
+                        high = mid;
+                    } else {
+                        low = mid + 1;
+                    }
+                }
+            }
+        }
+        return i;
+    }
+}
